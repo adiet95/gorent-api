f58d1925dc99f881af311be8ae500350cc733896
Adiet Alimudin <55053994+adiet95@users.noreply.github.com>
2022-10-01 20:32:33 +0700

    test

--- src/modules/v1/auth/auth_router.go
+++ src/modules/v1/auth/auth_router.go
@@ -12,7 +12,7 @@ func NewAu(rt *mux.Router, db *gorm.DB) {
 	svc := NewService(repo)
 	ctrl := NewCtrl(svc)
 
-	route.HandleFunc("", ctrl.SignIn).Methods("POST")
+	route.HandleFunc("/", ctrl.SignIn).Methods("POST")
 	route.HandleFunc("/register", ctrl.Register).Methods("POST")
 
 }
